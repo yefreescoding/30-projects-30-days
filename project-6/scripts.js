9b501f95055702aff7e17af19e19322f0837584f
Yefree Valdez <yefree13santos@outlook.com>
2023-08-02 17:59:17 +0200

    project done

--- project-6/scripts.js
+++ project-6/scripts.js
@@ -1,7 +1,13 @@
+// Calculator basic functionality
+
 const screenNumbers = document.getElementById('result');
 
 function appendToScreen(value) {
-  screenNumbers.value += value;
+  if (value === 'delete') {
+    screenNumbers.value = screenNumbers.value.slice(0, -1);
+  } else {
+    screenNumbers.value += value;
+  }
 }
 
 function clearScreen() {
@@ -10,5 +16,28 @@ function clearScreen() {
 
 function calculate() {
   const result = eval(screenNumbers.value);
-  screenNumbers.value = result;
+  screenNumbers.value = result.toLocaleString();
 }
+
+// Set Theme
+
+const themeToggle = document.querySelectorAll('.theme_toggle');
+
+themeToggle.forEach((toggle) => {
+  toggle.addEventListener('click', (e) => {
+    const root = document.documentElement;
+    const theme = e.target.getAttribute('data-theme');
+    root.className = theme;
+    localStorage.setItem('theme', theme);
+  });
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+  const savedTheme = localStorage.getItem('theme');
+  const root = document.documentElement;
+  if (savedTheme) {
+    root.className = savedTheme; // Apply the saved theme to the root element
+  }
+});
+
+document.querySelector('.theme-toggle').addEventListener('click', setTheme);
