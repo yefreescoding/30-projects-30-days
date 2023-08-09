51484e5de83a3f5c0bef243b2a14d079ccb45198
Yefree Valdez <yefree13santos@outlook.com>
2023-08-09 15:10:29 +0200

    add two new projects

---
+++ project-10/app/src/App.jsx
@@ -0,0 +1,9 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function App() {
+  return <div>test</div>;
+}
+
+export default App;
