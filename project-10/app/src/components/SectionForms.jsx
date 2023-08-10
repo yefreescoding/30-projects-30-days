5254dcbf142d4e7abfd492945dcd0440cf2f681e
Yefree Valdez <yefree13santos@outlook.com>
2023-08-10 15:53:46 +0200

    create form component

---
+++ project-10/app/src/components/SectionForms.jsx
@@ -0,0 +1,12 @@
+export default function SectionForm() {
+  return (
+    <form>
+      <span>35.000+ already joined</span>
+      <h2>Stay up-to-date with what we are doing</h2>
+      <div>
+        <input type="text" placeholder="Enter your email address" />
+        <button type="submit">Contact us</button>
+      </div>
+    </form>
+  );
+}
