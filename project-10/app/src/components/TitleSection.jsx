f2836596e7860aef1d1cfe844ddeb20f0965fcda
Yefree Valdez <yefree13santos@outlook.com>
2023-08-10 18:49:48 +0200

    style section titles

--- project-10/app/src/components/TitleSection.jsx
+++ project-10/app/src/components/TitleSection.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function TitleSection({ sectionTitle, description }) {
   return (
-    <div>
+    <div className="title_section">
       <h2 className="section__h2">{sectionTitle}</h2>
       <p className="section__p">{description}</p>
     </div>
