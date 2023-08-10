dd555098fe5c41cee757b03562670c9b15e18f4b
Yefree Valdez <yefree13santos@outlook.com>
2023-08-10 19:13:15 +0200

    style card component

--- project-10/app/src/components/Cards.jsx
+++ project-10/app/src/components/Cards.jsx
@@ -1,7 +1,17 @@
 export default function Cards() {
   return (
-    <article>
-      <h3>building</h3>
+    <article className="cards flex">
+      <div className="cards__info flex">
+        <img src="/images/logo-chrome.svg" alt="" />
+        <h3>Add to chrome</h3>
+        <p>Minimum version 62</p>
+      </div>
+      <div className="cards__action flex">
+        <img src="/images/bg-dots.svg" alt="" />
+        <a href="/" className="btn primary">
+          Add and install extension
+        </a>
+      </div>
     </article>
   );
 }
