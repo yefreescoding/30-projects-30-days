354a82663e3c5a81590d7d6fcd9ea99ea154991b
Yefree Valdez <yefree13santos@outlook.com>
2023-08-10 16:03:23 +0200

    add footer component

---
+++ project-10/app/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+// import components
+import HeaderLinks from './HeaderLinks';
+import links from '../data/links';
+// import images
+import bookMarkLogo from '/images/logo-bookmark.svg';
+import iconFacebook from '/images/icon-facebook.svg';
+import iconTwitter from '/images/icon-twitter.svg';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__top">
+        <div className="header__logo">
+          <img src={bookMarkLogo} alt="" className="header__logo-img" />
+        </div>
+        <nav className="footer__nav">
+          <ul className="footer__ul">
+            {links.links.map((link) => (
+              <HeaderLinks
+                key={link.name}
+                name={link.name}
+                href={link.href}
+                worth={true}
+              />
+            ))}
+          </ul>
+        </nav>
+        <div className="footer__logos">
+          <img src={iconFacebook} alt="" />
+          <img src={iconTwitter} alt="" />
+        </div>
+      </div>
+      <p className="attribution">
+        Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </p>
+    </footer>
+  );
+}
