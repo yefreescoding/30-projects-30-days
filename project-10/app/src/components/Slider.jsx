2458b67020099909c9ea48fd25124bd23ff200c4
Yefree Valdez <yefree13santos@outlook.com>
2023-08-11 19:15:20 +0200

    add slider animation

--- project-10/app/src/components/Slider.jsx
+++ project-10/app/src/components/Slider.jsx
@@ -4,36 +4,47 @@ import { useState } from 'react';
 import SliderTabs from './SliderTabs';
 // import data
 import tabsData from '../data/tabs.json';
+const tabsLink = [
+  {
+    name: 'Simple Bookmarking',
+    href: '#simple-bookmarking',
+  },
+  {
+    name: 'Speedy Searching',
+    href: '#speedy-searching',
+  },
+  {
+    name: 'Easy Sharing',
+    href: '#easy-sharing',
+  },
+];
 
 export default function Slider() {
-  const tabsLink = [
-    {
-      name: 'Simple Bookmarking',
-      href: '#simple-bookmarking',
-    },
-    {
-      name: 'Speedy Searching',
-      href: '#speedy-searching',
-    },
-    {
-      name: 'Easy Sharing',
-      href: '#easy-sharing',
-    },
-  ];
   const [activeTabIndex, setActiveTabIndex] = useState(0);
 
   const handleTabClick = (e, index) => {
     e.preventDefault();
     setActiveTabIndex(index);
+
+    const newLeft = index * 33.33 + '%'; // Adjust based on the number of tabs
+    e.currentTarget.style.setProperty('--_left', newLeft);
   };
 
   // let tabLabel = 1;
 
   return (
     <div className="slider flex flex-col">
-      <ul className="slider__ul flex" aria-labelledby="tabs-title">
+      <ul
+        className="slider__ul flex"
+        aria-labelledby="tabs-title"
+        style={{ '--_left': activeTabIndex * 33.33 + '%' }}
+      >
         {tabsLink.map((link, index) => (
-          <li key={link.href} className="slider__li">
+          <li
+            key={link.href}
+            className="slider__li"
+            aria-selected={activeTabIndex === index ? 'true' : 'false'}
+          >
             <a
               className="slider__a"
               onClick={(e) => handleTabClick(e, index)}
