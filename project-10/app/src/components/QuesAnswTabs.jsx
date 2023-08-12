081cbb4ae0560559e7e681e169de1f7d569a300b
Yefree Valdez <yefree13santos@outlook.com>
2023-08-12 12:08:40 +0200

    create new style file

--- project-10/app/src/components/QuesAnswTabs.jsx
+++ project-10/app/src/components/QuesAnswTabs.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/prop-types */
 export default function QuesAnswTabs({ question, answer, handleOpen }) {
   return (
-    <div>
+    <article>
       {' '}
-      <h3 onClick={handleOpen}>{question}</h3>
+      <h3 className="section_questions__h3 flex" onClick={handleOpen}>
+        {question}
+        <img src="/images/icon-arrow.svg" alt="" />
+      </h3>
       <p>{answer}</p>
-    </div>
+    </article>
   );
 }
--- project-10/app/src/components/QuestionsAnswers.jsx
+++ project-10/app/src/components/QuestionsAnswers.jsx
@@ -5,16 +5,14 @@ import infoData from '../data/questions.json';
 
 export default function QuestionsAnswers() {
   return (
-    <div>
-      <article className="">
-        {infoData.map((tab) => (
-          <QuesAnswTabs
-            key={tab.id}
-            question={tab.question}
-            answer={tab.answer}
-          />
-        ))}
-      </article>
+    <div className="section_questions__tabs">
+      {infoData.map((tab) => (
+        <QuesAnswTabs
+          key={tab.id}
+          question={tab.question}
+          answer={tab.answer}
+        />
+      ))}
     </div>
   );
 }
