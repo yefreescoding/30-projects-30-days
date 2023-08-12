147bf571b1f09f3e28468fe24fc0eba5a5d862ec
Yefree Valdez <yefree13santos@outlook.com>
2023-08-12 11:57:48 +0200

    create new component

---
+++ project-10/app/src/components/QuestionsAnswers.jsx
@@ -0,0 +1,3 @@
+export default function QuestionsAnswers() {
+  return <div>testing</div>;
+}
--- project-10/app/src/components/SectionQuestion.jsx
+++ project-10/app/src/components/SectionQuestion.jsx
@@ -1,13 +1,23 @@
+import QuestionsAnswers from './QuestionsAnswers';
 import TitleSection from './TitleSection';
 
 export default function SectionQuestions() {
   return (
-    <section className="section">
+    <section className="section" aria-labelledby="Frequently asked questions">
       <TitleSection
         sectionTitle="Frequently Asked Questions"
         description={`Here are some of our FAQs. If you have any other questions
                       you’d like answered please feel free to email us.`}
       />
+      <QuestionsAnswers />
+      <a
+        href="/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn primary"
+      >
+        More info
+      </a>
     </section>
   );
 }
