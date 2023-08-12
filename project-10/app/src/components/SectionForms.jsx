5b177b077f8aafc19818d00b7d5aa3e339a35048
Yefree Valdez <yefree13santos@outlook.com>
2023-08-12 11:40:43 +0200

    style form section

--- project-10/app/src/components/SectionForms.jsx
+++ project-10/app/src/components/SectionForms.jsx
@@ -1,11 +1,13 @@
 export default function SectionForm() {
   return (
-    <form className="form">
+    <form className="form flex flex-col">
       <span className="form__span">35.000+ already joined</span>
       <h2>Stay up-to-date with what we are doing</h2>
-      <div className="form__action">
-        <input type="text" placeholder="Enter your email address" />
-        <button type="submit">Contact us</button>
+      <div className="form__action flex">
+        <input type="email" placeholder="Enter your email address" />
+        <button className="btn-act" type="submit">
+          Contact us
+        </button>
       </div>
     </form>
   );
