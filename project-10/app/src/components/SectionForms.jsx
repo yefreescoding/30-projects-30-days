7c5f1b55ed121d2b160ce779f5dcc79dd2c52a2a
Yefree Valdez <yefree13santos@outlook.com>
2023-08-12 11:26:56 +0200

    add new style file

--- project-10/app/src/components/SectionForms.jsx
+++ project-10/app/src/components/SectionForms.jsx
@@ -1,9 +1,9 @@
 export default function SectionForm() {
   return (
-    <form>
-      <span>35.000+ already joined</span>
+    <form className="form">
+      <span className="form__span">35.000+ already joined</span>
       <h2>Stay up-to-date with what we are doing</h2>
-      <div>
+      <div className="form__action">
         <input type="text" placeholder="Enter your email address" />
         <button type="submit">Contact us</button>
       </div>
