97fa1fd904408a7dfe0ed4f3d3fe6babd7e86e13
Yefree Valdez <yefree13santos@outlook.com>
2023-08-24 12:00:00 +0200

    feat: add images and icons

--- project-18/app/src/components/header/header.jsx
+++ project-18/app/src/components/header/header.jsx
@@ -1,5 +1,52 @@
+// Component imports
+import ShoppingCart from '../shoppingCart/ShoppingCart';
+
+// images import
+const logo = '/icons/logo.svg/';
+
+// Navigation Links
+const navLinks = [
+  {
+    name: 'Collection',
+    href: '/',
+  },
+  {
+    name: 'Men',
+    href: '/',
+  },
+  {
+    name: 'Women',
+    href: '/',
+  },
+  {
+    name: 'About',
+    href: '/',
+  },
+  {
+    name: 'Contact',
+    href: '/',
+  },
+];
+
 function Header() {
-  return <h1>HEADER</h1>;
+  return (
+    <header className="header">
+      <a href="/" aria-label="Link home">
+        <img src={logo} alt="Logo of the website" />
+      </a>
+      <nav className="header__nav">
+        {navLinks.map((link) => (
+          <a className="header__link" key={link.name} href={link.href}>
+            {link.name}
+          </a>
+        ))}
+      </nav>
+      <ShoppingCart />
+      <div className="header__avatar">
+        <img src="" alt="" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
