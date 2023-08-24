c3dfd5f11aba93c0857bcc609a90fdd20d62b83b
Yefree Valdez <yefree13santos@outlook.com>
2023-08-24 18:29:03 +0200

    feat: product images added

--- project-18/app/src/components/productDisplay/ProductDisplay.jsx
+++ project-18/app/src/components/productDisplay/ProductDisplay.jsx
@@ -1,3 +1,18 @@
+// styles imports
+import styles from './productDisplay.module.css';
+
 export default function ProductDisplay() {
-  return <div>Product Images</div>;
+  return (
+    <section className={styles['product_images']}>
+      <div className={styles['product_images__cover']}>
+        <img src="/images/image-product-1.jpg" alt="" />
+      </div>
+      <div className={styles['product_images__select']}>
+        <img src="/images/image-product-1.jpg" alt="" />
+        <img src="/images/image-product-2.jpg" alt="" />
+        <img src="/images/image-product-3.jpg" alt="" />
+        <img src="/images/image-product-4.jpg" alt="" />
+      </div>
+    </section>
+  );
 }
